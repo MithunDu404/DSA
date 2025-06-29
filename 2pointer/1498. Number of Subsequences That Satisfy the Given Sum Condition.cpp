db363913f53a6a9e9c0c445f908d55b97758352c
Mithun Dutta <mithundu632@gmail.com>
2025-06-29 10:05:02 +0530

    1498_

--- 2pointer/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++ 2pointer/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/
 
+
+// With binary search
 class Solution {
 public:
     int M = 1e9+7;
@@ -21,6 +23,37 @@ public:
             int id = upper_bound(nums.begin(),nums.end(),v) - nums.begin() - 1;
             int k = id - i;
             ans = (int)(ans + power[k]) % M;
+        }
+
+        return ans;
+    }
+};
+
+// With two pointer
+
+class Solution {
+public:
+    int M = 1e9+7;
+    
+    int numSubseq(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+
+        vector<int> power(n);
+        power[0] = 1;
+        for(int i=1;i<n;i++){
+            power[i] = (power[i-1] + power[i-1])%M;
+        }
+
+        int ans = 0;
+        int l = 0;
+        int r = n-1;
+        while(l <= r){
+            if(nums[l] + nums[r] > target) r--;
+            else{
+                ans = (ans + power[r-l]) % M;
+                l++;
+            }
         }
 
         return ans;
